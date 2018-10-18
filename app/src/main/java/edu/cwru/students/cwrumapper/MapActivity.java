0598cfb0fb3406bfb4a4ba9c67c27f66813d7576
timothyjin <tjin316@gmail.com>
2018-10-18 00:24:09 -0400

    Added GoogleMap and map methods to MainActivity

--- app/src/main/java/edu/cwru/students/cwrumapper/MapActivity.java
+++
@@ -1,8 +0,0 @@
-package edu.cwru.students.cwrumapper;
-
-import android.support.v7.app.AppCompatActivity;
-
-import com.google.android.gms.maps.GoogleMap;
-
-public class MapActivity extends AppCompatActivity implements GoogleMap.OnMapLoadedCallback {
-}
