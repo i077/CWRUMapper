d39c1afdee718012a91dbdd390e4fa65f63ba72d
Yagmur <yagmur.eff1@gmail.com>
2018-11-02 05:10:17 -0400

    Buttons present, lacking response functionality. OnStart in MainActivity needs to set view to activity_sign_in in the future to open on login screen.

--- app/src/main/java/edu/cwru/students/cwrumapper/MainActivity.java
+++ app/src/main/java/edu/cwru/students/cwrumapper/MainActivity.java
@@ -1,13 +1,28 @@
 package edu.cwru.students.cwrumapper;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, GoogleApiClient.OnConnectionFailedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
